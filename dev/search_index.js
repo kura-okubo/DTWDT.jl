2cb926d087faf327266af71a4e6dcbfa80ea7f23
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-01 23:10:16 +0000

    build based on 03ff059

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#DTWDT.jl-1","page":"Home","title":"DTWDT.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [DTWDT]","category":"page"},{"location":"#DTWDT.test-Tuple{}","page":"Home","title":"DTWDT.test","text":"test() return 1\n\n\n\n\n\n","category":"method"}]
+}
