bbd6fa41d05c9e40768d9ba40ae4ff89a1c066bf
kura-okubo <kurama_okubo@fas.harvard.edu>
2019-06-01 18:08:32 -0400

    update

--- docs/make.jl
+++ docs/make.jl
@@ -3,12 +3,21 @@ push!(LOAD_PATH,"../src/")
 using Documenter, DTWDT
 
 makedocs(
-    modules=[DTWDT],
+    modules = [DTWDT],
+    format = Documenter.HTML(
+        # Use clean URLs, unless built as a "local" build
+        prettyurls = !("local" in ARGS),
+        canonical = "https://kura-okubo.github.io/DTWDT.jl/stable/",
+        assets = ["assets/favicon.ico"],
+    ),
+    clean = false,
     sitename="DTWDT.jl",
     authors="kurama",
-    pages=Any[
+    linkcheck = !("skiplinks" in ARGS),
+    pages = [
         "Home" => "index.md",
     ],
+    strict = true,
 )
 
 deploydocs(
